7fe56d1539dfa52b90457e37a93b12894c87f48a
TwistZz12 <twistzzmeng@gmail.com>
2025-02-27 18:14:34 +0000

    Re-upload Unity project without large files

---
+++ Assets/PlayerState.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState 
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    protected Rigidbody2D rb;
+
+
+    protected float xInput;
+    protected float yInput;
+    private string animBoolName;
+
+    protected float stateTimer;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string animBoolName)
+    {
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = animBoolName;
+
+    }
+
+    public virtual void Enter()
+    {
+        player.anim.SetBool(animBoolName,true);
+        rb = player.rb;
+    }
+
+
+    public virtual void Update() 
+    {
+        stateTimer -= Time.deltaTime;
+
+        xInput = Input.GetAxisRaw ("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+        player.anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+    public virtual void Exit()
+    {
+        
+        player.anim.SetBool(animBoolName,false);
+
+    }
+
+}
