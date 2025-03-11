2c055a2901c05bc89f100dadc082a175c696de62
TwistZz12 <twistzzmeng@gmail.com>
2025-03-11 18:03:24 +0000

    Training Ground setup and start  enemy state machine

---
+++ Assets/EnemyStateMachine.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine 
+{
+
+    public EnemyState currentState {  get; private set; }
+
+    public void Initialize(EnemyState _startState)
+    {
+
+
+    }
+}
