8841a79ffec2771f7bda5f50d4cf3c29349e710d
TwistZz12 <twistzzmeng@gmail.com>
2025-03-11 18:44:29 +0000

    Create entity to remove basic statemachine for better inheritance

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,9 +1,10 @@
+using Cinemachine.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D.IK;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
 
     [Header("Attack details")]
@@ -21,22 +22,14 @@ public class Player : MonoBehaviour
     public float dashDuration;
     public float dashDir {  get; private set; }
 
-    [Header("Collision info")]
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private Transform wallCheck;
-    [SerializeField] private float wallCheckDistance;
-    [SerializeField] private LayerMask whatIsGround;
+    
 
 
     public int facingDir { get; private set; } = 1;
     private bool facingRight = true;
 
 
-    #region Components
-    public Animator anim {  get; private set; }
-    public Rigidbody2D rb { get; private set; }
-    #endregion
+
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
 
@@ -56,8 +49,9 @@ public class Player : MonoBehaviour
 
     #endregion
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
         stateMachine = new PlayerStateMachine();
         
         idleState = new PlayerIdleState (this,stateMachine,"Idle");
@@ -69,18 +63,19 @@ public class Player : MonoBehaviour
         wallJump = new PlayerWallJumpState(this, stateMachine, "Jump");
         primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
     }
-    private void Start()
+    protected override void Start()
     {
 
-        anim = GetComponentInChildren<Animator> ();
-        rb = GetComponent<Rigidbody2D> ();
+        base.Start();
 
         stateMachine.Initialize(idleState);
 
     }
 
-    private void Update()
+    protected override void Update()
     {
+        base.Update();
+
         stateMachine.currentState.Update();
         
     
@@ -124,42 +119,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    #region Velocity
-    public void ZeroVelocity() => rb.velocity = new Vector2(0, 0);
-   
-    public void SetVelocity (float _xVelocity, float _yVelocity)
-    {
-        rb.velocity = new Vector2(_xVelocity, _yVelocity);
-        FlipContorller(_xVelocity);
 
-    }
-    #endregion
 
-    #region Collision
-    public bool isGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-    public bool isWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
-    }
-    #endregion
 
-    #region Flip
-    public void Flip()
-    {
-        facingDir = facingDir * -1;
-        facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
-    }
-
-    public void FlipContorller(float _x)
-    {
-        if (_x > 0 && !facingRight)
-            Flip();
-        else if (_x < 0 && facingRight)
-            Flip();
-    }
-    #endregion
+ 
 }
