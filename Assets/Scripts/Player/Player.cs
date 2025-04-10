03f8e6b958ac9dd8fc15bfc63ce7b13f88555fb7
TwistZz12 <twistzzmeng@gmail.com>
2025-04-10 22:29:08 +0100

    Finish regular and bounce throw sword skill

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -11,32 +11,26 @@ public class Player : Entity
     public Vector2[] attackMovement;
     public float counterAttackDuration = .2f;
 
-
-    public bool isBusy {  get; private set; }
+    public bool isBusy { get; private set; }
     [Header("Move Info")]
     public float moveSpeed = 12f;
     public float jumpForce;
+    public float swordReturnImpact;
 
     [Header("Dash Info")]
     public float dashSpeed;
     public float dashDuration;
-    public float dashDir {  get; private set; }
+    public float dashDir { get; private set; }
 
-
-    public SkillManager skill {  get; private set; }
-    
-
-
-    public int facingDir { get; private set; } = 1;
-    private bool facingRight = true;
-
+    public SkillManager skill { get; private set; }
+    public GameObject sword { get; private set; }
 
 
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
 
     public PlayerIdleState idleState { get; private set; }
-    public PlayerMoveState moveState {  get; private set; }
+    public PlayerMoveState moveState { get; private set; }
 
     public PlayerJumpState jumpState { get; private set; }
     public PlayerAirState airState { get; private set; }
@@ -50,15 +44,18 @@ public class Player : Entity
     public PlayerPrimaryAttackState primaryAttack { get; private set; }
     public PlayerCounterAttackState counterAttack { get; private set; }
 
+    public PlayerAimSwordState aimSword { get; private set; }
+    public PlayerCatchSwordState catchSword { get; private set; }
+
     #endregion
 
     protected override void Awake()
     {
         base.Awake();
         stateMachine = new PlayerStateMachine();
-        
-        idleState = new PlayerIdleState (this,stateMachine,"Idle");
-        moveState = new PlayerMoveState (this,stateMachine,"Move");
+
+        idleState = new PlayerIdleState(this, stateMachine, "Idle");
+        moveState = new PlayerMoveState(this, stateMachine, "Move");
         jumpState = new PlayerJumpState(this, stateMachine, "Jump");
         airState = new PlayerAirState(this, stateMachine, "Jump");
         dashState = new PlayerDashState(this, stateMachine, "Dash");
@@ -66,68 +63,58 @@ public class Player : Entity
         wallJump = new PlayerWallJumpState(this, stateMachine, "Jump");
         primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
         counterAttack = new PlayerCounterAttackState(this, stateMachine, "CounterAttack");
+
+        aimSword = new PlayerAimSwordState(this, stateMachine, "AimSword");
+        catchSword = new PlayerCatchSwordState(this, stateMachine, "CatchSword");
     }
     protected override void Start()
     {
-
         base.Start();
-
         skill = SkillManager.instance;
-
         stateMachine.Initialize(idleState);
-
     }
 
     protected override void Update()
     {
         base.Update();
-
         stateMachine.currentState.Update();
-        
-    
         CheckForDashInput();
+    }
+
+
+    public void AssignNewSword(GameObject _newSword)
+    {
+        sword = _newSword;
+    }
+
+    public void CatchTheSword()
+    {
+        stateMachine.ChangeState(catchSword);
+        Destroy(sword);
     }
 
 
     public IEnumerator BusyFor(float _seconds)
     {
         isBusy = true;
-
-        yield return new WaitForSeconds (_seconds);
-
+        yield return new WaitForSeconds(_seconds);
         isBusy = false;
     }
 
     public void AnimationTrigger() => stateMachine.currentState.AnimationFinishTrigger();
-    
-
-    
 
     private void CheckForDashInput()
     {
-
         if (isWallDetected())
             return;
 
-        
-
-
-        if (Input.GetKeyDown(KeyCode.LeftShift) && SkillManager.instance.dash.CanUseSkill( ))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && SkillManager.instance.dash.CanUseSkill())
         {
-           
             dashDir = Input.GetAxisRaw("Horizontal");
-
             if (dashDir == 0)
                 dashDir = facingDir;
-
 
             stateMachine.ChangeState(dashState);
         }
     }
-
-
-
-
-
- 
 }
